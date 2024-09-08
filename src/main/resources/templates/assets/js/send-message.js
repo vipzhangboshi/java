0d6cab0ca26ec7f05a54a1154fe2217477a73a8d
vipzhangboshi <42635421+vipzhangboshi@users.noreply.github.com>
2024-09-08 23:14:49 +0800

    Delete src directory

--- src/main/resources/templates/assets/js/send-message.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-export function sendPhoneAuthCode(data) {
-  return axios.post('/api/third/message/phone-code', data)
-}
-export function sendEmailAuthCode(data) {
-  return axios.post('/api/third/message/email-code', data)
-}
